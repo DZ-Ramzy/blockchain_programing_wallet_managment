c6fc93c071d472638c8aff84045fe158cc96373b
Ramzy <ramzychibani@gmail.com>
2026-09-16 14:50:01 +0200

    etape 3/4 : generation entropie 128 bits et representations binary/bytes/hex

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-def generate():
-    print("TODO: generate a phrase")
+import secrets
+
+ENTROPY_BITS = 128
+
 
 def import_phrase():
     print("TODO: import")
+
+
+def generate():
+    entropy = secrets.randbits(ENTROPY_BITS)
+
+    entropy_bits = format(entropy, f"0{ENTROPY_BITS}b")
+    entropy_bytes = entropy.to_bytes(ENTROPY_BITS // 8, "big")
+    entropy_hex = format(entropy, f"0{ENTROPY_BITS // 4}x")
+
+    print("binary :", entropy_bits)
+    print("bytes  :", entropy_bytes)
+    print("hex    :", entropy_hex)
+
+
 
 def main():
     while True:
